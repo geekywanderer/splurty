bcd6fd9daf6109e16c19a1be16a006020c2ea9b2
S. Tan <wanderlust@thosetans.com>
2015-08-24 14:53:14 +0000

    refactored code to pass test for unique_tag

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+
+  test "unique_tag" do
+    quote = Quote.create(:author => 'Say-Ho Tan', :saying => 'You only YOLO once.')
+    expected = 'ST#' + quote.id.to_s
+    actual = quote.unique_tag
+    assert_equal expected, actual
   end
+
+
 end
